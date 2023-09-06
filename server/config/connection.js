1800326b9e48d97150ec41a247d368d0b27151c9
Giovanne-Villanueva <gvv8106@gmail.com>
2023-09-05 18:32:10 -0700

    Fixing issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.ORMONGO_URL || 'mongodb://127.0.0.1:27017/text-adventure');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/text-adventure');
 
 module.exports = mongoose.connection;
